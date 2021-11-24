de7f86878f6c5e821f99c873507fa7778228293b
sauravsmn <saurav741@gmail.com>
2021-11-24 21:30:31 +0100

    commit

---
+++ emotion.py
@@ -0,0 +1,11 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+"""
+Created on Tue Nov 23 13:29:27 2021
+
+@author: saurav
+"""
+
+# Install Libraries
+!pip install textblob
+!pip install tweepy
---
+++ worldemotion.py
@@ -0,0 +1,5 @@
+import tweepy
+from textblob import Textblob
+import preprocessor
+import statistics
+from typing import List
